e2ab2db883b873f1f3c194548a62c7c1e6cb5efb
reenan <reenan.arbitrario@gmail.com>
2015-06-30 02:46:01 +0000

    make chart display price value

--- client/components/chart-stock/directive/chart-stock.directive.js
+++ client/components/chart-stock/directive/chart-stock.directive.js
@@ -18,35 +18,29 @@ angular.module('workspaceApp')
           if(chart.series.length === chart.stocks.length){
             element.highcharts('StockChart', {
 
-             rangeSelector: {
-                    selected: 30
-                },
+              rangeSelector: {
+                    selected: 4
+              },
 
-                yAxis: {
-                    labels: {
-                        formatter: function () {
-                            return (this.value > 0 ? ' + ' : '') + this.value + '%';
-                        }
-                    },
-                    plotLines: [{
-                        value: 0,
-                        width: 2,
-                        color: 'silver'
-                    }]
-                },
+              
+              yAxis: {
+                plotLines: [{
+                  value: 0,
+                  width: 2,
+                  color: 'silver'
+                }]
+              },
 
-                plotOptions: {
-                    series: {
-                        compare: 'percent'
-                    }
-                },
+              title: {
+                text: 'Stock IO'
+              },
 
-                tooltip: {
-                    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-                    valueDecimals: 2
-                },
+              tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                valueDecimals: 2
+              },
 
-                series: chart.series
+              series: chart.series
             });
           }
         }, true);
